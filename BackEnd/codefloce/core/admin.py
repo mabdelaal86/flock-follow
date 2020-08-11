b3f809804d9b3a6908a5a9393da28825c455aab5
AbdAllahAboElMakarem <abdallah.m.daly@gmail.com>
2020-08-11 16:33:02 +0200

    adjustment

--- BackEnd/codefloce/core/admin.py
+++ BackEnd/codefloce/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Messages, User # ,Flcks ##,Flock_user
+
+admin.site.register(Messages)
+admin.site.register(User)
+#admin.site.register(Flcks)
+##admin.site.register(Flock_user)

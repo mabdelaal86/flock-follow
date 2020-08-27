7212d79c2ac099f1a3b88d0de2ebb89a49fe5d87
AbdAllahAboElMakarem <abdallah.m.daly@gmail.com>
2020-08-28 00:16:25 +0200

    urls

--- BackEnd/codefloce/codefloce/urls.py
+++ BackEnd/codefloce/codefloce/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from core.views import Messages, Flock_User, Flcks, User
+
+
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('admin/', admin.site.urls),
+
+    #urls
+    path('messages/', Messages),
+    path('flock_user/', Flock_User),
+    path('flcks/', Flcks),
+    path('user/', User),
+
 ]

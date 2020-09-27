98dd78865e2af303859ea7af29b692faca020fca
Mohammed AbdelAal <mabdelaal86@gmail.com>
2020-09-27 16:35:49 +0200

    rename codefloce into flock_follow

--- backend/flock_follow/wsgi.py
+++ backend/flock_follow/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for codefloce project.
+WSGI config for flock_follow project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codefloce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flock_follow.settings')
 
 application = get_wsgi_application()

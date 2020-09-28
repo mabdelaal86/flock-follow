790adcd7c2076225c9d728cbb7123d6aad216fb8
Mohammed AbdelAal <mabdelaal86@gmail.com>
2020-09-28 20:30:47 +0200

    switch into HttpResponse in views

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class User(models.Model):
     name = models.CharField(max_length=20)
@@ -9,6 +8,7 @@ class User(models.Model):
 #    status =  models.()
     latitude = models.CharField(max_length=150)
     longitude = models.CharField(max_length=150)
+
 
 class Flcks(models.Model):
     title = models.CharField(max_length=25)
@@ -21,18 +21,13 @@ class Flcks(models.Model):
     leader_id = models.OneToOneField(User ,on_delete=models.CASCADE)
 
 
-
 class Messages(models.Model):
     date_time = models.DateTimeField(auto_now_add=True)
     contect = models.CharField(max_length=50)
-    user_id = models.ForeignKey(User ,on_delete=models.CASCADE)
-    flock_id = models.ForeignKey(Flcks ,on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    flock_id = models.ForeignKey(Flcks, on_delete=models.CASCADE)
+
 
 class Flock_User(models.Model):
-   user_id = models.ForeignKey(User ,on_delete=models.CASCADE)
-   flock_id = models.ForeignKey(Flcks ,on_delete=models.CASCADE)
-
-
-
-
-   
+   user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+   flock_id = models.ForeignKey(Flcks, on_delete=models.CASCADE)

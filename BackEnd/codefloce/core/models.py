1cf82ac5424652a24733d85218c33326163c992f
AbdAllahAboElMakarem <abdallah.m.daly@gmail.com>
2020-10-03 13:05:55 +0200

    linkBE&UI

--- BackEnd/codefloce/core/models.py
+++ BackEnd/codefloce/core/models.py
@@ -3,26 +3,26 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    name = models.CharField(max_length=20)
-    about = models.CharField(max_length=30)
-    phone = models.CharField(max_length=20) # no frequent
+    name = models.CharField(max_length=20, null=True)
+    about = models.CharField(max_length=30, null=True)
+    phone = models.CharField(max_length=20, null=True) # no frequent
 #    status =  models.()
-    latitude = models.CharField(max_length=150)
-    longitude = models.CharField(max_length=150)
+    latitude = models.CharField(max_length=150, null=True)
+    longitude = models.CharField(max_length=150, null=True)
 
 class Flcks(models.Model):
-    title = models.CharField(max_length=25)
-    description = models.CharField(max_length=101)
+    title = models.CharField(max_length=25, null=True)
+    description = models.CharField(max_length=101, null=True)
 #    status =  models.()
-    password = models.CharField(max_length=55)
-    destination = models.CharField(max_length=300)
-    latitude = models.CharField(max_length=150)
-    longitude = models.CharField(max_length=150)
+    password = models.CharField(max_length=55, null=True)
+    destination = models.CharField(max_length=300, null=True)
+    latitude = models.CharField(max_length=150, null=True)
+    longitude = models.CharField(max_length=150, null=True)
     leader_id = models.OneToOneField(User ,on_delete=models.CASCADE)
 
 class Messages(models.Model):
     date_time = models.DateTimeField(auto_now_add=True)
-    contect = models.CharField(max_length=50)
+    contect = models.CharField(max_length=50, null=True)
     user_id = models.ForeignKey(User ,on_delete=models.CASCADE)
     flock_id = models.ForeignKey(Flcks ,on_delete=models.CASCADE)
 
--- BackEnd/codefloce/core/views.py
+++ BackEnd/codefloce/core/views.py
@@ -4,16 +4,16 @@ from django.http import HttpResponse
 
 def Messages(request, *args, **kwargs):
     # return HttpResponse("<h1> Messages</h1>")
-    return render(request, {})
+    return render(request, 'mobileapp/lip/appbar.dart')
 
 def Flock_User(request, *args, **kwargs):
     #return HttpResponse("<h1> Flock_user </h1>")
-    return render(request, {})
+    return render(request, 'mobileapp/lip/')
 
 def Flcks(request, *args, **kwargs):
     #return HttpResponse("<h1> Flcks </h1>")
-    return render(request, {})
+    return render(request, 'mobileapp/lip/AddFlock.dart')
 
 def User(request, *args, **kwargs):
     #return HttpResponse("<h1> User </h1>")
-    return render(request, {})
+    return render(request, 'mobileapp/lip/settings.dart')

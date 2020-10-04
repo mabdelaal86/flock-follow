213cb6940589267abd7cb7075b22685d794ee5ce
Mohammed AbdelAal <mabdelaal86@gmail.com>
2020-10-04 23:30:42 +0200

    redefine models

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,32 +2,46 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=20)
-    about = models.CharField(max_length=30)
-    phone = models.CharField(max_length=20) # no frequent
-#    status =  models.()
-    latitude = models.CharField(max_length=150)
-    longitude = models.CharField(max_length=150)
+    USER_STATUSES = (
+        ('R', 'Registered'),
+        ('A', 'Approved'),  # If phone is approved
+    )
+
+    name = models.CharField(max_length=100)
+    about = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=50, unique=True)
+    status = models.CharField(max_length=1, choices=USER_STATUSES, default='R')
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Flcks(models.Model):
-    title = models.CharField(max_length=25)
-    description = models.CharField(max_length=101)
-#    status =  models.()
-    password = models.CharField(max_length=55)
-    destination = models.CharField(max_length=300)
-    latitude = models.CharField(max_length=150)
-    longitude = models.CharField(max_length=150)
-    leader_id = models.OneToOneField(User ,on_delete=models.CASCADE)
+class Flock(models.Model):
+    FLOCK_STATUSES = (
+        ('S', 'Standby'),
+        ('R', 'Running'),
+        ('F', 'Finished'),
+    )
+
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    status = models.CharField(max_length=1, choices=FLOCK_STATUSES, default='S')
+    password = models.CharField(max_length=20, blank=True)
+    destination = models.CharField(max_length=200, blank=True)
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    created_at = models.DateTimeField(auto_now_add=True)
+    started = models.DateTimeField(blank=True, null=True)
+    finished = models.DateTimeField(blank=True, null=True)
+
+    leader = models.ForeignKey(User, related_name='created_flocks', on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, related_name='joined_flocks')
 
 
-class Messages(models.Model):
+class Message(models.Model):
     date_time = models.DateTimeField(auto_now_add=True)
-    contect = models.CharField(max_length=50)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    flock_id = models.ForeignKey(Flcks, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-
-class Flock_User(models.Model):
-   user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-   flock_id = models.ForeignKey(Flcks, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
+    flock = models.ForeignKey(Flock, related_name='messages', on_delete=models.CASCADE)

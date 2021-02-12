1cddad89b874c36c28478c5767a71faff98eb7c6
abdallah <49618004+AbdAllahAboElMakarem@users.noreply.github.com>
2021-02-12 11:05:17 +0200

    Update settings.py

--- backend/flock_follow/settings.py
+++ backend/flock_follow/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    #Library import
     'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',

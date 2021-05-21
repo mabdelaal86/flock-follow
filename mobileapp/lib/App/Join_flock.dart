c5351d0bdd8c0b55f1cc7047846195983198985e
AbdAllahAboElMakarem <abdallah.m.daly@gmail.com>
2021-05-21 17:27:43 +0200

    flock

--- mobileapp/lib/App/Join_flock.dart
+++ mobileapp/lib/App/Join_flock.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flock_follow/App/home.dart';
+import 'package:flock_follow/App/appbar.dart';
 
 class JoinFlock extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return addflock(); //erre name
+    return _JoinFlock(); //erre name
   }
 }
 
-class addflock extends State {
+class _JoinFlock extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,26 +19,32 @@ class addflock extends State {
           Text('Join a Flock'),
           Text('To join/view this flock you povide a password.'),
           Text('Please contact the flock leader for the passwrd.'),
-          TextField(
+          TextFormField(
             controller: null,
             decoration: InputDecoration(icon: Icon(null), hintText: 'Password'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some Password';
+              }
+              return null;
+            },
           ),
           Row(
             children: [
               Center(
-                child: RaisedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/main');
-                  },
-                  child: Text('CANCEL'),
+                child: TextButton(
+                  onPressed: () => Navigator.of(context).pushNamed('/home'),
+                  child: const Text('Cancel'),
                 ),
               ),
               Center(
-                child: RaisedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/appbar');
-                  },
-                  child: Text('JOIN'),
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () => Navigator.of(context).pushNamed('/appbar'),
+                  child: const Text('JOIN'),
                 ),
               ),
             ],
